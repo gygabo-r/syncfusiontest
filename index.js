5c391487ee550d578dc4f7dba82315938d7eb4a0
gyegabor <gyetvai.gabor@gmail.com>
2021-05-11 22:45:47 +0200

    autowrap

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { render } from 'react-dom';
 import './index.css';
 import * as React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Edit, Inject, VirtualScroll } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Edit, Inject, VirtualScroll, ColumnChooser, Toolbar } from '@syncfusion/ej2-react-grids';
 import { data } from './data';
 import { SampleBase } from './sample-base';
 export class BatchEdit extends SampleBase {
     constructor() {
         super(...arguments);
-        this.toolbarOptions = ['Add', 'Delete', 'Update', 'Cancel'];
+        this.toolbarOptions = ['ColumnChooser'];
         this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Batch' };
         this.editparams = { params: { popupHeight: '300px' } };
         this.validationRule = { required: true };
@@ -18,7 +18,8 @@ export class BatchEdit extends SampleBase {
         return (<div className='control-pane'>
         <div className='control-section'>
           <div className='col-md-9'>
-              <GridComponent dataSource={data} editSettings={this.editSettings} height='400' allowTextWrap={true}>
+              <GridComponent dataSource={data} editSettings={this.editSettings} height='400' allowTextWrap={true} toolbar={this.toolbarOptions}
+              showColumnChooser={true}>
                 <ColumnsDirective>
                   <ColumnDirective field='OrderID' headerText='Order ID' width='120' textAlign='Right' validationRules={this.orderidRules} isPrimaryKey={true}></ColumnDirective>
                   <ColumnDirective field='CustomerName' headerText='Customer Name' width='150' validationRules={this.validationRule}></ColumnDirective>
@@ -26,7 +27,7 @@ export class BatchEdit extends SampleBase {
                   <ColumnDirective field='OrderDate' headerText='Order Date' editType='datepickeredit' format='yMd' width='170'></ColumnDirective>
                   <ColumnDirective field='ShipCountry' headerText='Ship Country' width='150' editType='dropdownedit' edit={this.editparams}></ColumnDirective>
                 </ColumnsDirective>
-                <Inject services={[VirtualScroll, Edit]}/>
+                <Inject services={[VirtualScroll, Edit, Toolbar, ColumnChooser]}/>
               </GridComponent>
             </div>
 
